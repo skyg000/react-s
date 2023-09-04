d3369da249dc5e3e7eba79d554b61caf2700f69e
skyg000 <skyg00700@gmail.com>
2023-09-04 12:46:41 +0900

    09-04
    server 사용

--- ex-front/src/App.js
+++ ex-front/src/App.js
@@ -1,12 +1,13 @@
 
 import axios from 'axios';
+
 function App() {
 
-  axios.get('http://localhost:3030/abc?id=100')
+  axios.get(`${process.env.REACT_APP_SERVER}abc`)
   .then(res=>{
     console.log(res);
   })
-  axios.post('http://localhost:3030/insert',{id:10000,name:'신규'})
+  /* axios.post('http://localhost:3030/insert',{id:10000,name:'신규'}) */
   
 
   return (
