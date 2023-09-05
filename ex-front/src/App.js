82bcfe5d06328c210f24b957cc6e2833f2eb4f37
skyg000 <skyg00700@gmail.com>
2023-09-05 10:11:11 +0900

    d

--- ex-front/src/App.js
+++ ex-front/src/App.js
@@ -1,5 +1,31 @@
 
 import axios from 'axios';
+/* 여러 곳에서 사용하지 않으면 App.js에서 한번에 사용 가능 */
+function List() {
+  return(
+        <li>
+          쓴글
+          <button> 삭제 </button>
+        </li>
+  )
+}
+
+function Write() {
+  const insert = (e)=>{
+    e.preventDefault();
+    let msg = e.target.msg.value;
+    axios.post(`${process.env.REACT_APP_SERVER}/insert`,{msg})
+    console.log(e.target.msg.value);
+  }
+  return(
+      <div>
+        <form onSubmit={insert}>
+          <input type="text" name="msg"/>
+          <input type="submit" value="저장"/>
+        </form>
+      </div>
+  )
+}
 
 function App() {
 
@@ -11,9 +37,13 @@ function App() {
   
 
   return (
-    <>
-    
-    </>
+    <div>
+      <h2> 한줄 댓글</h2>
+        <Write/>
+      <ul>
+        <List/>
+      </ul>
+    </div>
   );
 }
 
